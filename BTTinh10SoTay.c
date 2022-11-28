2fbf4ebba0ccd6fd3d99eb06fa45db971e315f1a
Nhat Minh <117696796+LeTranNhatMinh@users.noreply.github.com>
2022-11-28 08:44:28 +0700

    Update BTTinh10SoTay.c

--- BTTinh10SoTay.c
+++ BTTinh10SoTay.c
@@ -2,12 +2,10 @@
 
 #include<stdio.h>
 
-// Tổng (S) tiền 10 cuốn sổ tay.
-
 int main(){
-    int S,N; // Khai báo biến S.
+    int S,N; // Khai báo biến S, N.
     S = (500 * 10); // Tính tổng 10 cuốn sổ tay giá gốc.
-    N = S - (S* 20 / 100); // Tính tổng 10 cuốn sổ tay sau khi giảm giá 20%.
-    printf("Tong tien 10 cuon so tay gia goc la: %d", S); // In ra tổng 10 cuốn sổ tay giá gốc.
-    printf("\nTong tien 10 cuon so tay sau khi giam 20%% la: %d", N); // In ra tổng số tiền sau khi giảm 20%.
+    N = S - (S * 20 / 100); // Tính tổng 10 cuốn sổ tay sau khi giảm giá 20%.
+    printf("Tong tien 10 cuon so tay gia goc la: %d", S); // In ra tổng giá tiền 10 cuốn sổ tay giá gốc.
+    printf("\nTong tien 10 cuon so tay sau khi giam 20%% la: %d", N); // In ra tổng giá tiền sau khi giảm 20%.
 }
